bf418ce5856d7b9018714ef7200a3e4a0fed562c
juliana <julianageorgeeshak@gmail.com>
2024-12-05 01:11:07 +0200

    edit Main

--- src/main/java/org/os/Main.java
+++ src/main/java/org/os/Main.java
@@ -23,34 +23,72 @@ public class Main {
         System.out.println("3. SRTF Scheduler");
         System.out.println("4. FCAI Scheduler");
         int Case = input.nextInt();
-        for (int i = 0; i < numProcess; i++) {
-            System.out.println("Please enter name of process " + (i+1));
-            ProcessName = input.next();
-            System.out.println("Please enter Color of process " + ProcessName);
-            ProcessColor=input.next();
-            System.out.println("Please enter arrival time of process " + ProcessName);
-            ArrivalTime = input.nextInt();
-            System.out.println("Please enter burst time of process " + ProcessName);
-            BurstTime = input.nextInt();
-            System.out.println("Please enter priority Number of Process "+ ProcessName);
-            priorityNum = input.nextInt();
-            processes.add(new Process(ProcessName,ProcessColor, ArrivalTime, BurstTime, priorityNum));}
         switch (Case){
             case 1:
+                for (int i = 0; i < numProcess; i++) {
+                    System.out.println("Please enter name of process " + (i+1));
+                    ProcessName = input.next();
+                    System.out.println("Please enter Color of process " + ProcessName);
+                    ProcessColor=input.next();
+                    System.out.println("Please enter arrival time of process " + ProcessName);
+                    ArrivalTime = input.nextInt();
+                    System.out.println("Please enter burst time of process " + ProcessName);
+                    BurstTime = input.nextInt();
+                    System.out.println("Please enter priority Number of Process "+ ProcessName);
+                    priorityNum = input.nextInt();
+                    processes.add(new Process(ProcessName,ProcessColor, ArrivalTime, BurstTime, priorityNum));}
                 System.out.println("Please enter context switching time ");
                 contextSwitchTime = input.nextInt();
                 Priority_Scheduling priorityScheduling=new Priority_Scheduling(processes,contextSwitchTime);
                 break;
             case 2:
+                for (int i = 0; i < numProcess; i++) {
+                    System.out.println("Please enter name of process " + (i+1));
+                    ProcessName = input.next();
+                    System.out.println("Please enter Color of process " + ProcessName);
+                    ProcessColor=input.next();
+                    System.out.println("Please enter arrival time of process " + ProcessName);
+                    ArrivalTime = input.nextInt();
+                    System.out.println("Please enter burst time of process " + ProcessName);
+                    BurstTime = input.nextInt();
+                    System.out.println("Please enter priority Number of Process "+ ProcessName);
+                    priorityNum = input.nextInt();
+                    processes.add(new Process(ProcessName,ProcessColor, ArrivalTime, BurstTime, priorityNum));}
                 System.out.println("Please enter context switching time ");
                 contextSwitchTime = input.nextInt();
                 SJF sjf=new SJF(processes,contextSwitchTime);
                 break;
             case 3:
-
+                for (int i = 0; i < numProcess; i++) {
+                    System.out.println("Please enter name of process " + (i+1));
+                    ProcessName = input.next();
+                    System.out.println("Please enter Color of process " + ProcessName);
+                    ProcessColor=input.next();
+                    System.out.println("Please enter arrival time of process " + ProcessName);
+                    ArrivalTime = input.nextInt();
+                    System.out.println("Please enter burst time of process " + ProcessName);
+                    BurstTime = input.nextInt();
+                    System.out.println("Please enter priority Number of Process "+ ProcessName);
+                    priorityNum = input.nextInt();
+                    processes.add(new Process(ProcessName,ProcessColor, ArrivalTime, BurstTime, priorityNum));}
                 SRTF srtf=new SRTF(processes);
                 break;
             case 4:
+                for (int i = 0; i < numProcess; i++) {
+                    System.out.println("Please enter name of process " + (i+1));
+                    ProcessName = input.next();
+                    System.out.println("Please enter Color of process " + ProcessName);
+                    ProcessColor=input.next();
+                    System.out.println("Please enter arrival time of process " + ProcessName);
+                    ArrivalTime = input.nextInt();
+                    System.out.println("Please enter burst time of process " + ProcessName);
+                    BurstTime = input.nextInt();
+                    System.out.println("Please enter priority Number of Process "+ ProcessName);
+                    priorityNum = input.nextInt();
+                System.out.println("Please enter Quantum time ");
+                RRTimeQuantum = input.nextInt();
+                processes.add(new Process(ProcessName,ProcessColor, ArrivalTime, BurstTime, priorityNum,RRTimeQuantum));
+                }
                 FCAI fcai =new FCAI(processes);
                 break;
             default:
