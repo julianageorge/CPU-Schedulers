19aafde6d1c633b82314c9ef457218d78df1b186
juliana <121284395+julianageorge@users.noreply.github.com>
2024-12-04 12:35:53 +0200

    Make Project Structure

---
+++ src/main/java/CPU/Schedulers/Priority_Scheduling.java
@@ -0,0 +1,10 @@
+package CPU.Schedulers;
+
+import CPU.Process;
+
+import java.util.ArrayList;
+
+public class Priority_Scheduling {
+    public Priority_Scheduling(ArrayList<Process> processes, int contextSwitchTime) {
+    }
+}
